5e018e0e8c6167dfe86a72df59a046a13a71312f
Ian Ehlert <ian.ehlert@tstmedia.com>
2012-08-28 11:22:36 -0500

    Setting up pull_requests and issues methods on base

--- lib/github/base.rb
+++ lib/github/base.rb
@@ -1,13 +1,14 @@
 module Github
   class Base
     def initialize(args={})
-      # work out the default app_root
-      app_root = args[:app_root] || '.'
+      if args[:config]
+        @config = args[:config]
+      else
+        config_file = args[:config_file] || '/config/github.yml'
 
-      config_file = args[:config_file] || '/config/github.yml'
-
-      raise config_file+" not found" unless File.exist? config_file
-      @config = YAML.load(File.open config_file)
+        raise config_file+" not found" unless File.exist? config_file
+        @config = YAML.load(File.open config_file)
+      end
     end
 
     def config
@@ -17,6 +18,14 @@ module Github
     def base_url
       base = @config['repo_organization'] || @config['repo_user']
       "https://api.github.com/repos/#{base}/#{@config['repo']}"
+    end
+
+    def pull_requests
+      @pull_requests ||= PullRequests.new(@config)
+    end
+
+    def issues
+      @issues ||= Issues.new(@config)
     end
 
     def api_request(api_url, http_method='GET', params={})
